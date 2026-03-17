a8f911521ec0779e72086981de3796e75d424e5d
Avadhut4244 <patilavadhut9960@gmail.com>
2026-03-18 03:35:59 +0530

    Clean structure - removed duplicate and target

--- voyagepro-backend-main/voyagepro-backend/voyagepro-backend/src/main/java/com/voyagepro/voyagepro_backend/repository/TravelPackageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.voyagepro.voyagepro_backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.voyagepro.voyagepro_backend.model.TravelPackage;
-
-public interface TravelPackageRepository extends JpaRepository<TravelPackage, Long> {
-
-}
